25c85de87323ebb8cb5650fdab9fb48bf8e9f26d
Wendeley Fernandes <wendeleyti@gmail.com>
2019-09-14 17:57:01 -0300

    Criando repositório

---
+++ dev-quest/Assets/Scripts/GameSession.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField] int playerLives = 3;
+    [SerializeField] Text livesText;
+    [SerializeField] Player player;
+
+    private void Awake() {
+        int numGameSession = FindObjectsOfType<GameSession>().Length;
+        if(numGameSession > 1) {
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        livesText.text = playerLives.ToString();
+	}
+	
+    public void ProcessPlayerDeath() {
+        if(playerLives > 1) {
+            TakeLife();
+        }
+        else {
+            ResetGameSession();
+        }
+    }
+
+    private void TakeLife() {
+        playerLives--;
+        var CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+
+    private void ResetGameSession() {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            //Save
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            //Load
+            Load();
+        }
+    }
+
+    private void Save() {
+        SaveObject save = new SaveObject {
+            positionx = player.transform.position.x,
+            positiony = player.transform.position.y,
+            scene = SceneManager.GetActiveScene().buildIndex
+        };
+        string json = JsonUtility.ToJson(save);
+        Debug.Log(json);
+        File.WriteAllText(Application.dataPath + "/save.txt", json);
+
+    }
+
+    private void Load() {
+        if(File.Exists(Application.dataPath + "/save.txt")) {
+            string saveString = File.ReadAllText(Application.dataPath + "/save.txt");
+            SaveObject save = JsonUtility.FromJson<SaveObject>(saveString);
+            player.transform.position = new Vector2(save.positionx, save.positiony);
+        }
+    }
+
+    private class SaveObject {
+        public int scene;
+        public float positionx;
+        public float positiony;
+    }
+}
---
+++ dev-quest/Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    // Parâmetros de configuração
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
+
+    // Estados
+    bool isAlive = true;
+
+    Rigidbody2D playerRigidBody;
+    Animator playerAnimator;
+    CapsuleCollider2D playerBodyCollider2D;
+    BoxCollider2D playerFeetCollider;
+    float gravityScaleAtStart;
+
+    void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        playerFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = playerRigidBody.gravityScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isAlive) { return; }
+        Run();
+        ClimbLadder();
+        Jump();
+        FlipSprite();
+        Die();
+	}
+
+    private void Run() {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //Valor entre -1 e +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void ClimbLadder() {
+        if (!playerFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            playerAnimator.SetBool("Climbing", false );
+            playerRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(playerRigidBody.velocity.x, controlThrow * climbSpeed);
+        playerRigidBody.velocity = climbVelocity;
+        playerRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+    }
+
+    private void Jump() {
+
+        if (!playerFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump")){
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            playerRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void Die() {
+        if (playerBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy","Hazards"))) {
+            isAlive = false;
+            playerAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+
+    private void FlipSprite() {
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed) {
+            // Se o jogador estiver movendo na horizontal
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1f);
+        }
+
+    }
+}
