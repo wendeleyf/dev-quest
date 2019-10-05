55b17e4f3fc0c4a99a3aa1924c408358064fbf89
Wendeley Fernandes <wendeleyti@gmail.com>
2019-10-05 20:29:52 -0300

    Add sistema de inventário

---
+++ dev-quest/Assets/Scripts/Pickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject item;
+
+    private void Start() {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            for (int i = 0; i < inventory.slots.Length; i++) {
+                if(inventory.isFull[i] == false) {
+                    inventory.isFull[i] = true;
+                    Instantiate(item, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
+
